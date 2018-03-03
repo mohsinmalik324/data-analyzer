d407dd3f22ac8789180cac0b908b7b6b3418995b
CurtisChris7 <curtischris7@gmail.com>
2018-03-03 16:43:57 -0500

    Add files via upload
    Contains primary object prototype

---
+++ app/sample_object.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+
+class sample_object:
+
+    def __init__(self,file):
+        self.csv_file = open(file)
+        self.dataframe = pd.DataFrame.from_csv(self.csv_file)
+
+    def csv_has_read(file):
+        f = open("file", 'r')
+        pd.read_csv()
+        print("Successfully Loaded!")
+
+    def get_csv_file(self):
+        return self.csv_file
+
+    def display_csv_file(self):
+        print(pd.read_csv(self.csv_file))
+
+    def get_data_frame(self):
+        return self.dataframe
+
+    def display_data_frame(self):
+        print(self.dataframe)
+
+    def get_mean_column1(self):
+        return self.dataframe.mean()
